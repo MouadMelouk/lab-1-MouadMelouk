18dc1ba475d10bf1990230b9f564524a4cb4926c
MouadMelouk <120326795+MouadMelouk@users.noreply.github.com>
2023-09-05 10:46:15 +0000

    ex2

--- lab1exercise2.cpp
+++ lab1exercise2.cpp
@@ -11,9 +11,9 @@ Factorial of a number n is defined as:
 // Recursive function to compute factorial
 int factorial(int n) {
     if (n == 0 || n == 1) {
-        // Fill appropriately
+        return n;
     } else {
-        // Fill appropriately with a recursive cae
+        return factorial(n-1) * n;
     }
 }
 
