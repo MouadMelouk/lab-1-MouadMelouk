314b05847ed5b653e803e3fad06ae07896ba6fc4
MouadMelouk <120326795+MouadMelouk@users.noreply.github.com>
2023-09-05 11:24:30 +0000

    ex8

--- lab1exercise8.cpp
+++ lab1exercise8.cpp
@@ -6,22 +6,24 @@ namespace used to call the function.*/
 #include <iostream>
 
 // Namespace for Rectangle
-/*Declare the namespace for the appropriate below Area Calculation*/ {
+/*Declare the namespace for the appropriate below Area Calculation*/
+namespace Rectangle {
     double calculateArea(double length, double width) {
         return length * width;
     }
 }
 
 // Namespace for Triangle
-/*Declare the namespace for the appropriate below Area Calculation*/ {
+/*Declare the namespace for the appropriate below Area Calculation*/ 
+namespace Triangle{
     double calculateArea(double base, double height) {
         return 0.5 * base * height;
     }
 }
 
 int main() {
-    double rectangleArea = /*Call the function with the appropriate Namespace*/calculateArea(4.0, 3.0);
-    double triangleArea = /*Call the function with the appropriate Namespace*/calculateArea(5.0, 6.0);
+    double rectangleArea = Rectangle::calculateArea(4.0, 3.0);     /*Call the function with the appropriate Namespace*/
+    double triangleArea = Triangle::calculateArea(5.0, 6.0);      /*Call the function with the appropriate Namespace*/
 
     std::cout << "Area of Rectangle: " << rectangleArea << std::endl;
     std::cout << "Area of Triangle: " << triangleArea << std::endl;
